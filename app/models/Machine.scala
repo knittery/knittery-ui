1691b2e1ae086e7356f63a1787f2dcef7922c2d0
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-15 22:43:52 +0100

    Implement position tracking.

--- app/models/Machine.scala
+++ app/models/Machine.scala
@@ -20,19 +20,28 @@ class Machine(connectorProps: Props) extends Actor {
   var subscribers = Set.empty[ActorRef]
   def notify(msg: Any) = subscribers.foreach(_ ! msg)
 
+  var positions = Map.empty[CarriageType, CarriagePosition]
+
   override def receive = {
     case Subscribe =>
       subscribers += sender
       context watch sender
+      sender ! Subscribed
     case Unsubscribe =>
       subscribers -= sender
 
+    case GetPositions =>
+      sender ! Positions(positions)
+
     //MachineEvents
-    case PositionUpdate(pos, direction, carriage) =>
-      ???
+    case PositionUpdate(pos, direction, Some(carriage)) =>
+      notify(PositionChanged(carriage, pos))
+      positions += carriage -> pos
+    case PositionUpdate(_, _, _) => () //ignore
 
     case Terminated if sender == connector => //Connector crashed
       //TODO handle the crash
+      //TODO reset the positions and requery somehow?
       ()
     case Terminated => // subscriber terminates
       subscribers -= sender
@@ -46,4 +55,10 @@ object Machine {
 
   case object Subscribe extends Command
   case object Unsubscribe extends Command
+
+  case object Subscribed extends Event
+  case class PositionChanged(carriage: CarriageType, position: CarriagePosition) extends Event
+
+  case object GetPositions extends Command
+  case class Positions(positions: Map[CarriageType, CarriagePosition]) extends Event
 }
