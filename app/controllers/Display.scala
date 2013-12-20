b0d31b569fdd78f53d722cc229ed757e4134c87d
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-20 01:18:33 +0100

    Live update the log on the display page.

--- app/controllers/Display.scala
+++ app/controllers/Display.scala
@@ -3,6 +3,8 @@ package controllers
 import scala.concurrent.duration._
 import play.api.Play._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.iteratee._
 import play.api.libs.concurrent.Akka
 import play.api.libs.concurrent.Execution.Implicits._
 import akka.actor._
@@ -27,6 +29,54 @@ object Display extends Controller {
     }
   }
 
-  def subscribe = TODO
+  def subscribe = WebSocket.using[JsValue] { req =>
+    val jsE = Subscription.enumerator &> Enumeratee.map {
+      case PositionChanged(carriage, pos) =>
+        val c = carriage match {
+          case KCarriage => "K"
+          case LCarriage => "L"
+          case GCarriage => "G"
+        }
+        val p = pos match {
+          case CarriageLeft(i) => Json.obj("where" -> "left", "overlap" -> i)
+          case CarriageRight(i) => Json.obj("where" -> "right", "overlap" -> i)
+          case CarriageOverNeedles(i) => Json.obj("where" -> "right", "needle" -> i.number, "needleIndex" -> i.index)
+        }
+        val json: JsValue = Json.obj(
+          "event" -> "positionChange", "carriage" -> c, "position" -> p)
+        json
+    }
+    (Iteratee.ignore, jsE)
+  }
 
+  object Subscription {
+    def enumerator = e
+
+    private val (e, channel) = Concurrent.broadcast[PositionChanged]
+    Akka.system.actorOf(Props(new SubscriptionActor), "display-subscription")
+
+    class SubscriptionActor extends Actor with ActorLogging {
+      override def preStart = {
+        log.debug("Subscribing to machine")
+        machine ! Subscribe
+      }
+      override def postStop = {
+        machine ! Unsubscribe
+      }
+      override def receive = {
+        case Subscribed =>
+          log.debug("Subscription established")
+          context become subscribed(sender)
+      }
+      def subscribed(to: ActorRef): Receive = {
+        case event: PositionChanged =>
+          log.info(s"Received $event")
+          channel push event
+        case Terminated if sender == to =>
+          log.debug(s"Resubscribing, $sender has crashed")
+          machine ! Subscribe
+          context become receive
+      }
+    }
+  }
 }
