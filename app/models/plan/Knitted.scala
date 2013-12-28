51b0e175c72bec813e7fc57b2bcf698ea2fa51b4
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-28 16:01:08 +0100

    Add knitting output.

---
+++ app/models/plan/Knitted.scala
@@ -0,0 +1,38 @@
+package models.plan
+
+case class Knitted(rows: Seq[KnittedRow]) {
+  if (rows.size > 0) require(rows.forall(_.width == rows(0).width), "Non-uniform width")
+
+  def height = rows.size
+  def width = {
+    if (rows.size == 0) throw new IllegalStateException("Unknown width.")
+    rows(0).width
+  }
+
+  def +(row: KnittedRow) = copy(rows :+ row)
+
+  def patternString = rows.reverse.mkString("\n")
+  override def toString = patternString
+}
+object Knitted {
+  def empty = Knitted(Seq.empty)
+}
+
+case class KnittedRow(stiches: Seq[Stich]) {
+  def width = stiches.size
+  def patternString = stiches.map(_.patternString).mkString
+  override def toString = patternString
+}
+
+sealed trait Stich {
+  def patternString: String
+}
+case object NoStrich extends Stich {
+  override def patternString = " "
+}
+case class PlainStich(yarn: Yarn) extends Stich {
+  override def patternString = "^"
+}
+case class PurlStich(yarn: Yarn) extends Stich {
+  override def patternString = "-"
+}
--- app/models/plan/KnittingState.scala
+++ app/models/plan/KnittingState.scala
@@ -4,9 +4,11 @@ import models._
 
 case class KnittingState(needles: NeedlePatternRow,
   yarnA: Option[Yarn], yarnB: Option[Yarn],
-  carriages: Map[CarriageType, (CarriageSettings, CarriagePosition)])
+  carriages: Map[CarriageType, (CarriageSettings, CarriagePosition)],
+  output: Knitted) {
+}
 
 object KnittingState {
-  val initial = KnittingState(_ => NeedleA, None, None, Map.empty)
+  val initial = KnittingState(_ => NeedleA, None, None, Map.empty, Knitted.empty)
 }
 
