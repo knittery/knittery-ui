57118d3cad19d0fae4753fd214c22a357791f72c
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-28 09:56:54 +0100

    Page call tests for simulator and display.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -15,15 +15,29 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in new WithApplication {
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
+    }
+
+    "render the serial-simulator page" in new WithApplication {
+      val page = route(FakeRequest(GET, "/simulator")).get
+
+      status(page) must equalTo(OK)
+      contentType(page) must beSome.which(_ == "text/html")
+    }
+
+    "render the display page" in new WithApplication {
+      val page = route(FakeRequest(GET, "/display")).get
+
+      status(page) must equalTo(OK)
+      contentType(page) must beSome.which(_ == "text/html")
     }
   }
 }
