97925bd8970584843f93d26fa136d3ffd4592395
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-29 13:38:25 +0100

    Add matrix type.

---
+++ app/utils/package.scala
@@ -0,0 +1,24 @@
+package object utils {
+  type Matrix[A] = IndexedSeq[IndexedSeq[A]]
+
+  implicit class RichMatrix[A](val matrix: Matrix[A]) extends AnyVal {
+    import RichMatrix._
+    def width = matrix.headOption.map(_.size).getOrElse(0)
+    def height = matrix.size
+    def rows = matrix
+    def columns = transpose
+    def transpose: IndexedSeq[IndexedSeq[A]] = matrix match {
+      case m: TransposedMatrix[A] => m.matrix //don't make a chain of wrapping
+      case matrix => new TransposedMatrix(matrix)
+    }
+    def validate = {
+      if (matrix.nonEmpty) matrix.foreach(r => require(r.size == width, "row widths are non-equal"))
+    }
+  }
+  private object RichMatrix {
+    private class TransposedMatrix[A](val matrix: Matrix[A]) extends IndexedSeq[IndexedSeq[A]] {
+      override def apply(index: Int): IndexedSeq[A] = matrix.map(_(index))
+      override def length = matrix.width
+    }
+  }
+}
