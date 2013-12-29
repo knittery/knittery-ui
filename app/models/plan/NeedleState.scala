81d8cdeb90776a94d11b244a9fb2e2f5df35f40c
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-29 19:02:28 +0100

    Check for invalid needle state.

--- app/models/plan/NeedleState.scala
+++ app/models/plan/NeedleState.scala
@@ -3,5 +3,7 @@ package models.plan
 import models.NeedlePosition
 
 case class NeedleState(position: NeedlePosition, yarn: Option[Yarn] = None) {
+  if (yarn.isDefined) require(position.isWorking, "Cannot have yarn on non working needle.")
+
   override def toString = s"$position(${yarn.getOrElse("_")})"
 }
