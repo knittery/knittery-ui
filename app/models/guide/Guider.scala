a2c0f7a5fdbb925777687b0fa3c97ed00faf9abd
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-01-03 18:40:16 +0100

    Add subscribtions to Guider.

--- app/models/guide/Guider.scala
+++ app/models/guide/Guider.scala
@@ -13,11 +13,20 @@ object Guider {
   case class CommandExecuted(command: Command) extends Event
   case class CommandNotExecuted(command: Command, reason: String) extends Event
 
+  /** Move to the next step. Answer: Command[Not]Executed. */
   case object Next extends Command
+  /** Move to the previous step. Answer: Command[Not]Executed. */
   case object Previous extends Command
 
+  /** Get the current step. Answer: CurrentStep. */
   case object QueryStep extends Command
   case class CurrentStep(step: GuideStep) extends Event
+
+  /** Subscribe to ChangeEvent. Answer: Command[Not]Executed. */
+  case object Subscribe extends Command
+  case class ChangeEvent(newStep: GuideStep) extends Event
+  /** Unsubscribe from ChangeEvent. Answer: Command[Not]Executed. */
+  case object Unsubscribe extends Command
 
   private class Guider(plan: Plan) extends Actor {
     override def preStart = {
@@ -27,23 +36,42 @@ object Guider {
       case step: GuideStep => context become stepping(step)
     }
 
+    var subscribers = Set.empty[ActorRef]
+    def notify(msg: Any) = subscribers.foreach(_ ! msg)
+
+    def changeStep(newStep: GuideStep) = {
+      notify(ChangeEvent(newStep))
+      context become (stepping(newStep))
+    }
+
     def stepping(step: GuideStep): Receive = {
       case QueryStep => sender ! CurrentStep(step)
 
-      case Next =>
+      case cmd @ Next =>
         if (!step.isLast) {
-          context become (stepping(step.next))
-          sender ! CommandExecuted(Next)
+          changeStep(step.next)
+          sender ! CommandExecuted(cmd)
         } else {
-          sender ! CommandNotExecuted(Next, "Already at last step")
+          sender ! CommandNotExecuted(cmd, "Already at last step")
         }
-      case Previous =>
+      case cmd @ Previous =>
         if (!step.isFirst) {
-          context become (stepping(step.previous))
-          sender ! CommandExecuted(Previous)
+          changeStep(step.previous)
+          sender ! CommandExecuted(cmd)
         } else {
-          sender ! CommandNotExecuted(Previous, "Already at first step")
+          sender ! CommandNotExecuted(cmd, "Already at first step")
         }
+
+      case cmd @ Subscribe =>
+        subscribers = subscribers + sender
+        context watch sender
+        sender ! CommandExecuted(cmd)
+      case cmd @ Unsubscribe =>
+        subscribers = subscribers - sender
+        context unwatch sender
+        sender ! CommandExecuted(cmd)
+      case Terminated if subscribers.contains(sender) =>
+        subscribers = subscribers - sender
     }
   }
 
