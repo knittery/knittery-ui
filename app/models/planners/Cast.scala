2acbcefc2c3862a77548f6773c781198b35ff650
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-01-12 21:37:39 +0100

    Only cast of needles that have yarn on it.

--- app/models/planners/Cast.scala
+++ app/models/planners/Cast.scala
@@ -9,6 +9,8 @@ object Cast {
   def onClosed(from: Needle, until: Needle, withYarn: Yarn): Planner =
     ClosedCastOn(from, until, withYarn)
 
-  def offClosed(withYarn: Yarn, filter: Needle => Boolean = _ => true): Planner =
-    ClosedCastOff(withYarn, filter)
+  def offClosed(withYarn: Yarn, filter: Needle => Boolean = _ => true): Planner = for {
+    needleState <- Planner.state(_.needles)
+    _ <- ClosedCastOff(withYarn, n => filter(n) && needleState(n).yarn.nonEmpty)
+  } yield ()
 }
