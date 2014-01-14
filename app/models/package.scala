b0574faf3ae4ef2a70c0697e2a71a45395ffbda0
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-01-14 22:51:35 +0100

    Add NeedleActionRow.invert.

--- app/models/package.scala
+++ app/models/package.scala
@@ -9,6 +9,12 @@ package object models {
   type NeedleActionRow = Needle => NeedleAction
   val AllNeedlesToB: NeedleActionRow = _ => NeedleToB
   val AllNeedlesToD: NeedleActionRow = _ => NeedleToD
+  implicit class RichNeedleActionRow(val row: NeedleActionRow) extends AnyVal {
+    def invert: NeedleActionRow = n => row(n) match {
+      case NeedleToB => NeedleToD
+      case NeedleToD => NeedleToB
+    }
+  }
 
   implicit class RichNeedleFun[A](val f: Needle => A) extends AnyVal {
     def all = Needle.all.map(f)
