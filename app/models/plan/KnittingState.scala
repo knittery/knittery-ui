c2dbb63c1e1669048a654cd870e47c4229bca7e3
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-01-18 15:58:20 +0100

    Update KnittingState to use CarriageState.

--- app/models/plan/KnittingState.scala
+++ app/models/plan/KnittingState.scala
@@ -5,38 +5,49 @@ import Scalaz._
 import models._
 
 case class KnittingState(needles: NeedleStateRow,
-  yarnA: Option[Yarn], yarnB: Option[Yarn],
-  carriageSettings: Map[Carriage, CarriageSettings],
-  carriagePosition: Map[Carriage, CarriagePosition],
+  carriageState: CarriageStates,
   output: Knitted) {
 
   def workingNeedles = Needle.all.filter(needles(_).position.isWorking)
-  def nextDirection(carriage: Carriage) = carriagePosition.get(carriage) match {
+  def nextDirection(carriage: Carriage) = carriageState.get(carriage).map(_.position) match {
     case Some(pos) =>
       val overlappedWorking = carriage.over(pos).filter(needles(_).position.isWorking)
       if (overlappedWorking.nonEmpty) s"carriage still over working needeles ${overlappedWorking.mkString(",")}".fail
       else pos.directionTo(workingNeedles.headOption.getOrElse(Needle.middle)).success
     case None => ToRight.success
   }
-  def yarns: Set[Yarn] = yarnA.toSet ++ yarnB.toSet
 
-  def modifyCarriageSettings(settings: CarriageSettings) =
-    copy(carriageSettings = carriageSettings + (settings.carriage -> settings))
-  def moveCarriage(carriage: Carriage, pos: CarriagePosition) =
-    copy(carriagePosition = carriagePosition + (carriage -> pos))
-  def moveCarriage(carriage: Carriage, direction: Direction): KnittingState =
-    moveCarriage(carriage, if (direction == ToLeft) CarriageLeft(0) else CarriageRight(0))
-  def moveCarriage(carriage: Carriage, to: LeftRight): KnittingState = to match {
-    case Left => moveCarriage(carriage, ToLeft)
-    case Right => moveCarriage(carriage, ToRight)
-  }
   def moveNeedles(positions: Needle => NeedlePosition) =
     modifyNeedles(Needle.all.map(n => n -> needles(n).copy(position = positions(n))).toMap)
   def modifyNeedles(newNeedles: NeedleStateRow) = copy(needles = newNeedles)
+
   def knit(f: Needle => Stitch) = copy(output = output + f)
 }
-
 object KnittingState {
-  val initial = KnittingState(_ => NeedleState(NeedleA), None, None, Map.empty, Map.empty, Knitted.empty)
+  val initial = KnittingState(_ => NeedleState(NeedleA), CarriageStates.empty, Knitted.empty)
 }
 
+sealed trait CarriageStates {
+  protected val data: Map[Carriage, CarriageState]
+  def +(state: CarriageState) = {
+    val data2 = data + (carriageFor(state) -> state)
+    new CarriageStates {
+      override val data = data2
+    }
+  }
+  def apply(carriage: Carriage) = {
+    data.get(carriage).getOrElse(carriage.initialState).
+      asInstanceOf[carriage.State]
+  }
+
+  private def carriageFor(state: CarriageState) = state match {
+    case _: KCarriage.State => KCarriage
+    case _: LCarriage.State => LCarriage
+    case _: GCarriage.State => GCarriage
+  }
+}
+object CarriageStates {
+  def empty = new CarriageStates {
+    override val data = Map.empty
+  }
+}
