8b4ab63bce718a0babff4a236d7dfa5a3ce8e490
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-01-20 20:01:21 +0100

    Add draft of YarnPoint.

--- app/models/Yarn.scala
+++ app/models/Yarn.scala
@@ -7,3 +7,15 @@ case class Yarn(name: String, color: Color) {
   def consoleColor = ConsoleColors.closest(color)
   override def toString = name
 }
+
+sealed trait YarnD {
+  def yarn: Yarn
+  def next = YarnPoint(yarn, this)
+  def previous: Option[YarnD]
+}
+case class YarnPoint(yarn: Yarn, prev: YarnD) extends YarnD {
+  override def previous = Some(prev)
+}
+case class YarnStart(yarn: Yarn) extends YarnD {
+  override def previous = None
+}
