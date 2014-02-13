af9b0272da524e83505d597fcffc0c005ec00cb4
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-02-13 14:05:48 +0100

    Plus and Minus on needle so we no longer need to use needle.index as much.

--- app/models/Needle.scala
+++ app/models/Needle.scala
@@ -5,6 +5,10 @@ sealed trait Needle extends Ordered[Needle] {
   def index: Int
   /** Number as shown on the machine (-100 to 100, 0 does not exist, negative are yellow). */
   def number: Int
+
+  def +(i: Int) = Needle.atIndex(index + i)
+  def -(i: Int) = +(-i)
+  def -(other: Needle) = index - other.index
 }
 
 object Needle {
