8b14c18396689840b556c53fe546dfa4284fb0fb
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-03-12 22:05:30 +0100

    Async layout 3d model as soon as plan is loaded in the Guider.

--- app/models/guide/Guider.scala
+++ app/models/guide/Guider.scala
@@ -1,9 +1,12 @@
 package models.guide
 
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scalaz._
-import Scalaz._
 import akka.actor._
-import models._
+import akka.pattern.{ask,pipe}
+import akka.util.Timeout
+import ch.inventsoft.graph.layout.Layout
 import models.plan._
 import models.machine._
 import utils.SubscriptionActor
@@ -26,6 +29,10 @@ object Guider {
   /** Get the current step. Answer: CurrentStep. */
   case object QueryStep extends Command
   case class CurrentStep(step: GuideStep) extends Event
+
+  /** Get 3D Layout as soon as ready. Answer: Knitted3DLayout. */
+  case object GetKnitted3D extends Command
+  case class Knitted3DLayout(knitted: Knitted3D, layout: Layout[Stitch3D]) extends Event
 
   /** Subscribe to ChangeEvent. Answer: Command[Not]Executed. */
   case object Subscribe extends Command
@@ -51,27 +58,36 @@ object Guider {
     override def receive = {
       case cmd @ LoadPlan(plan) =>
         val step = GuideStep(plan)
-        context become stepping(step)
+        val output3D = step.last.stateAfter.output3D
+        val layouter = context actorOf Layouter.props(output3D)
+        context become stepping(step, layouter)
         sender ! CommandExecuted(cmd)
     }
 
     var subscribers = Set.empty[ActorRef]
     def notify(msg: Any) = subscribers.foreach(_ ! msg)
 
-    def changeStep(newStep: GuideStep) = {
+    def changeStep(newStep: GuideStep, layouter: ActorRef) = {
       notify(ChangeEvent(newStep))
       newStep.step match {
         case KnitRow(_, _, pattern) =>
           machine ! Machine.LoadNeedlePattern(pattern)
         case _ => ()
       }
-      context become stepping(newStep)
+      context become stepping(newStep, layouter)
     }
 
     private var row = 0
 
-    def stepping(step: GuideStep): Receive = receive orElse {
+    def stepping(step: GuideStep, layouter: ActorRef): Receive = receive orElse {
       case QueryStep => sender ! CurrentStep(step)
+
+      case GetKnitted3D =>
+        implicit val timeout: Timeout = 10.minutes
+        (layouter ? Layouter.Get).map {
+          case layout: Layout[Stitch3D] =>
+            Knitted3DLayout(step.last.stateAfter.output3D, layout)
+        }.pipeTo(sender)
 
       case Machine.PositionChanged(_, _, old) if old == row =>
         ()
@@ -80,19 +96,19 @@ object Guider {
         if (!step.isLast) {
           val newStep = step.allFromHere.tail.
             find(_.isKnitting).getOrElse(step.last)
-          changeStep(newStep)
+          changeStep(newStep, layouter)
         }
 
       case cmd @ Next =>
         if (!step.isLast) {
-          changeStep(step.next)
+          changeStep(step.next, layouter)
           sender ! CommandExecuted(cmd)
         } else {
           sender ! CommandNotExecuted(cmd, "Already at last step")
         }
       case cmd @ Previous =>
         if (!step.isFirst) {
-          changeStep(step.previous)
+          changeStep(step.previous, layouter)
           sender ! CommandExecuted(cmd)
         } else {
           sender ! CommandNotExecuted(cmd, "Already at first step")
---
+++ app/models/guide/Layouter.scala
@@ -0,0 +1,60 @@
+package models.guide
+
+import scala.concurrent.duration._
+import akka.actor.{ActorLogging, Props, Actor}
+import models.plan.{Knitted3D, Stitch3D}
+import ch.inventsoft.graph.layout._
+import ch.inventsoft.graph.layout.spring.BarnesHutLayout
+
+private object Layouter {
+  sealed trait Command
+  sealed trait Event
+
+  /** Get the current layout. Will be answered with a Layout[Stitch3D] */
+  case object Get extends Command
+  /** Abort the layouting. Will be answered with a Layout[Stitch3D] */
+  case object Abort extends Command
+
+  /** Will be sent to the parent when layouting is done. */
+  case class Done(layout: Layout[Stitch3D]) extends Event
+
+  private case object Improve extends Command
+
+  def props(knitted: Knitted3D) = {
+    val layout = BarnesHutLayout(knitted.asGraph, knitted.asLayout, 1d)
+    Props(new Layouter(layout, 2000, knitted.asGraph.nodes.size))
+  }
+
+
+  private class Layouter(initial: IncrementalLayout[Stitch3D], limit: Int, size: Int) extends Actor with ActorLogging {
+    var stepNr = 0
+    var layout: IncrementalLayout[Stitch3D] = initial
+
+    private val startTime = System.currentTimeMillis
+    def since = (System.currentTimeMillis - startTime).millis
+
+    override def preStart() = {
+      log.debug(s"Will layout $size nodes..")
+      self ! Improve
+    }
+
+    override def receive = {
+      case Improve =>
+        layout = layout.improve
+        stepNr = stepNr + 1
+        if (stepNr < limit) {
+          self ! Improve
+        } else {
+          log.debug(s"Done layouting $size nodes after $stepNr steps in $since")
+          println(s"Done layouting $size nodes after $stepNr steps in $since")
+          context.parent ! Done(layout)
+        }
+
+      case Get =>
+        sender ! layout
+      case Abort =>
+        sender ! layout
+        context stop self
+    }
+  }
+}
