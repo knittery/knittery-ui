8a1ca3fea34426e1ce40026f8191ada856677fa7
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2015-07-20 13:13:09 +0200

    Factor out ProjectAction.

--- knittery-project-api/app/controllers/ProjectController.scala
+++ knittery-project-api/app/controllers/ProjectController.scala
@@ -15,8 +15,16 @@ import scala.concurrent.duration._
 
 class ProjectController @Inject()(@Named("project-repository") projectRepository: ActorRef)(implicit ec: ExecutionContext) extends Controller {
   implicit val timeout: Timeout = 1.second
-
   implicit val projectInfoFormat = Json.format[ProjectInfo]
+
+  class ProjectRequest[A](val projectId: UUID, val project: ActorRef, request: Request[A]) extends WrappedRequest[A](request)
+  def ProjectAction(projectId: UUID) = new ActionRefiner[Request, ProjectRequest] {
+    def refine[A](input: Request[A]) = (projectRepository ? GetProject(projectId)).map {
+      case ProjectFound(`projectId`, project) => Right(new ProjectRequest(projectId, project, input))
+      case ProjectNotFound(`projectId`) => Left(NotFound(s"Project $projectId does not exist."))
+    }
+  }
+  def ActionOnProject(id: UUID) = Action andThen ProjectAction(id)
 
   def create() = Action.async {
     (projectRepository ? CreateProject).map {
@@ -26,14 +34,9 @@ class ProjectController @Inject()(@Named("project-repository") projectRepository
     }
   }
 
-  def get(id: UUID) = Action.async {
-    (projectRepository ? GetProject(id)).flatMap {
-      case ProjectFound(`id`, project) =>
-        (project ? GetInfo).map {
-          case info: ProjectInfo => Ok(Json.toJson(info))
-        }
-      case ProjectNotFound(`id`) =>
-        Future.successful(NotFound(s"Project $id does not exist."))
+  def get(id: UUID) = ActionOnProject(id).async { req =>
+    (req.project ? GetInfo).map {
+      case info: ProjectInfo => Ok(Json.toJson(info))
     }
   }
 }
