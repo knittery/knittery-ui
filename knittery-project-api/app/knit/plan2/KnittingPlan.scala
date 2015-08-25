ae697dc2f20879a117832e998d5076b28687859c
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2015-08-25 20:33:32 +0200

    Add easy alias for pure.

--- knittery-project-api/app/knit/plan2/KnittingPlan.scala
+++ knittery-project-api/app/knit/plan2/KnittingPlan.scala
@@ -10,6 +10,9 @@ object KnittingPlan {
   //cannot use FreeC, because type inference for implicits will diverge
   type KnittingPlan[A] = Free[KnittingPlanF, A]
 
+  /** Alias for value.pure[KnittingPlan] */
+  def value[A](value: A): KnittingPlan[A] = value.pure[KnittingPlan]
+
   /** Knits a row using the specified carriage. */
   def knitRow(carriage: Carriage, direction: Direction, pattern: NeedleActionRow = AllNeedlesToB): KnittingPlan[Unit] =
     Free.liftFC(KnitRow(carriage, direction, pattern))
