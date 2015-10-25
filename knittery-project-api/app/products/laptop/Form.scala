9cbe2048388d39662f5a86abd04e9f9551035939
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2015-10-25 08:42:33 +0100

    Mark side and foldover (hidden) in the laptop knitting.

--- knittery-project-api/app/products/laptop/Form.scala
+++ knittery-project-api/app/products/laptop/Form.scala
@@ -16,9 +16,9 @@ object Form {
   def apply(width: Length, height: Length, topGap: Length, lash: Length, thickness: Length, patterns: Dimensions => Patterns)
     (implicit gauge: Gauge): Planner = for {
     _ <- Planner.preconditions(_ => true)
-    border = 2.stitches //used sew together front and back
+    border = 1.stitches //used sew together front and back
 
-    bodyWidth = (width + thickness).toStitches.discardPartials + border
+    bodyWidth = (width + thickness).toStitches.discardPartials + border * 2
     dims = Dimensions(
       bodyWidth = bodyWidth,
       frontHeight = (height + thickness / 2 - topGap).toRows.discardPartials - 1.rows,
@@ -26,6 +26,7 @@ object Form {
       lashWidth = bodyWidth - ((bodyWidth - width.toStitches) / 2).discardPartials * 2,
       lashHeight = lash.toRows.discardPartials
     )
+    sideSize = (thickness.toStitches + border) / 2
 
     ps <- Planner.preconditions { _ =>
       dims.checkPatterns(patterns(dims))
@@ -43,7 +44,17 @@ object Form {
     _ <- FairIslePlanner.singleBed(ps.front, Some(first))
     _ <- Basics.markLastRow(KnittingMark("front/back"))
     _ <- FairIslePlanner.singleBed(ps.back.reverse, Some(first))
+
+    _ <- Basics.markStitch(KnittingMark("left-side"),
+      first + sideSize.approx, MainBed, (dims.frontHeight + dims.backHeight).approx)
+    _ <- Basics.markStitch(KnittingMark("right-side"),
+      last - sideSize.approx, MainBed, (dims.frontHeight + dims.backHeight).approx)
+    hiddenFilter = (n: Needle) =>
+      (n >= first && n < (first + border.approx)) || (n <= last && n > last - border.approx)
+    _ <- Basics.markStitches(KnittingMark("hidden"),
+      hiddenFilter, MainBed, (dims.frontHeight + dims.backHeight).approx)
     _ <- Basics.markLastRow(KnittingMark("back/lash"))
+
     _ <- (0 until toDecrease).toVector.traverse { i =>
       for {
         working <- Planner.state(_.workingNeedles(MainBed).size)
